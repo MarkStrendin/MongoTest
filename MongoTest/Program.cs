e56846e3b16c96ca864eb7d3f80484aec0a2b6d2
Mark Strendin <mark.strendin@gmail.com>
2020-05-29 14:20:51 -0600

    Add MongoDB nuget package, and some basic code to list the databases on a server. I realize the connection string shouldn't be in the code like that - this is just a local test server that will be self-destructed after this experiment.

--- MongoTest/Program.cs
+++ MongoTest/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using MongoDB.Bson;
+using MongoDB.Driver;
 
 namespace MongoTest
 {
@@ -6,7 +9,16 @@ namespace MongoTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Connecting...");
+            MongoClient dbClient = new MongoClient("mongodb://10.0.0.120");
+            List<BsonDocument> dbList = dbClient.ListDatabases().ToList();
+            Console.WriteLine("Databases:");
+            foreach(BsonDocument db in dbList)
+            {
+                Console.WriteLine(db);
+            }
+
+
         }
     }
 }
